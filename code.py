699939da963e730a0d758114320247701cba2ce1
Youmn Hussien <youmn.hussien97@gmail.com>
2023-05-01 22:44:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,23 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn.metrics
-import yfinance as yf
-import pandas_ta as ta
-
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dropout
-from keras.layers import Dense
-from keras.layers import TimeDistributed
-
-import tensorflow as tf
-import keras
+from tensorflow import keras
 from keras import optimizers
-from keras.callbacks import History
 from keras.models import Model
-from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
+from keras.layers import Dense, LSTM, Input, Activation
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 backcandles = 30
@@ -31,13 +19,12 @@ def process_training_data(data):
         ['output_own_profits', 'output_own_cost', 'output_comp_price', 'output_X', 'output_own_share', 'output_own_sales',
          'output_own_price']]
     data_set = data.iloc[:, 0:6]  # .values without the price
-    pd.set_option('display.max_columns', None)
     sc = MinMaxScaler(feature_range=(0, 1))
     data_set_scaled = sc.fit_transform(data_set)
     data['output_own_price'] .shift(-1)
     data.dropna(inplace= True  )
     data.reset_index(inplace= True)
-    target = data.iloc[:, 6]
+    target = data.iloc[:, 7]
     X = []
     for j in range(6):
         X.append([])
@@ -47,6 +34,7 @@ def process_training_data(data):
     X = np.moveaxis(X, [0], [2])
     X, yi = np.array(X), np.array(target[backcandles:])
     y = np.reshape(yi, (len(yi), 1))
+
     return X, y
 
 
@@ -58,12 +46,11 @@ def split_data_train_test(X, y):
 def train_model(X_train, y_train):
     lstm_input = Input(shape=(backcandles, 6), name='lstm_input')
     inputs = LSTM(150, name='first_layer')(lstm_input)
-    # inputs = LSTM(100, return_sequences=True, name='Second_layer')(inputs)
-    # inputs = Dense(15, name='Second_dense_layer')(inputs)
+    inputs = Dense(100, name='Second_dense_layer')(inputs)
     inputs = Dense(1, name='dense_layer')(inputs)
     output = Activation('linear', name='output')(inputs)
     model = Model(inputs=lstm_input, outputs=output)
-    adam = optimizers.Adam()
+    adam = optimizers.Adam(learning_rate=0.001)
     model.compile(optimizer=adam, loss='mse')
     model.fit(x=X_train, y=y_train, batch_size=10, epochs=30, shuffle=True, validation_split=0.1)
     model.save("pricing_optimization_model")
@@ -74,19 +61,20 @@ def predict_price(X_test, y_test, model):
     pd.DataFrame(y_pred).to_csv('predicted_prices.csv')
     Score = sklearn.metrics.r2_score(y_test, y_pred)
     plt.figure(figsize=(10, 5))
-    plt.plot(y_test, color='black', label='Test')
-    plt.plot(y_pred, color='green', label='pred')
+    plt.plot(y_test, color='black', label='true price')
+    plt.plot(y_pred, color='green', label='predicted price')
+    plt.title("predicted price on true price")
     plt.legend()
     plt.show()
     plt.plot(y_pred-y_test, color='black', label='y_pred-y_test')
-    # plt.ylim([-1, 1])
+    plt.ylim([-1, 1])
+    plt.title("difference between predicted price and true price")
     plt.show()
-    print(y_pred)
-    print("the score is : ")
+    print("the score is : ") 
     print(Score)
 
 
-data = pd.read_csv(r'C:\Users\youmn\Downloads\output_data.csv') # please place here the path of data on your pc
+data = pd.read_csv(r'E:\2onn\output_data.csv') # please place here the path of data on your pc
 
 x, y = process_training_data(data)
 X_train, y_train, X_test, y_test = split_data_train_test(x, y)
@@ -96,6 +84,3 @@ if trained == 0:
 else:
     model = keras.models.load_model("pricing_optimization_model")
 predict_price(X_test, y_test, model)
-
-
-
